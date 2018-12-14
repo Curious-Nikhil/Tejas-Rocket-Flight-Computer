0d47008f10eec0e7975926d4cad97511952e9202
Nikhil Mishra <12347532+Curious-Nikhil@users.noreply.github.com>
2018-12-14 22:21:21 +0530

    Phase 1

---
+++ Flight Controller/ADXL_335_Sensor/ADXL_335_Sensor.ino
@@ -0,0 +1,46 @@
+// these constants describe the pins. They won't change:
+const int xpin = A0;                 // x-axis of the accelerometer
+const int ypin = A1;                  // y-axis
+const int zpin = A2;                  // z-axis (only on 3-axis models)
+const int LED = 13;
+
+void setup() {
+  // initialize the serial communications:
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+
+  // Provide ground and power by using the analog inputs as normal digital pins.
+  // This makes it possible to directly connect the breakout board to the
+  // Arduino. If you use the normal 5V and GND pins on the Arduino,
+  // you can remove these lines.
+//  pinMode(groundpin, OUTPUT);
+//  pinMode(powerpin, OUTPUT);
+//  digitalWrite(groundpin, LOW);
+//  digitalWrite(powerpin, HIGH);
+}
+
+void loop() {
+ 
+  if (analogRead(zpin) >450) {
+      digitalWrite(LED, HIGH);
+  delay(1000);
+  }else{
+      digitalWrite(LED, LOW);
+  delay(1000);
+  }
+
+  
+  
+  // print the sensor values:
+  Serial.print(analogRead(xpin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(ypin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(zpin));
+  Serial.println();
+  // delay before next reading:
+  delay(100);
+}
+
---
+++ Flight Controller/Kalman_Filter_Design/Curie_IMU.ino
@@ -0,0 +1,181 @@
+/* Copyright (C) 2012 Kristian Lauszus, TKJ Electronics. All rights reserved.
+
+ This software may be distributed and modified under the terms of the GNU
+ General Public License version 2 (GPL2) as published by the Free Software
+ Foundation and appearing in the file GPL2.TXT included in the packaging of
+ this file. Please note that GPL2 Section 2[b] requires that all works based
+ on this software must also be made publicly available under the terms of
+ the GPL2 ("Copyleft").
+
+ Contact information
+ -------------------
+
+ Kristian Lauszus, TKJ Electronics
+ Web      :  http://www.tkjelectronics.com
+ e-mail   :  kristianl@tkjelectronics.com
+ */
+
+#include <Wire.h>
+#include "Kalman.h" // Source: https://github.com/TKJElectronics/KalmanFilter
+#include "CurieIMU.h"
+
+//#define RESTRICT_PITCH // Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
+
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+
+float varX,varY,minX,maxX,minY,maxY;
+/* IMU Data */
+float accX, accY, accZ;
+float gyroX, gyroY, gyroZ;
+
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+int i;
+uint32_t timer;
+
+// TODO: Make calibration routine
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  CurieIMU.begin();
+  CurieIMU.setAccelerometerRange(2);
+  CurieIMU.setGyroRange(250);
+ 
+
+
+  delay(100); // Wait for sensor to stabilize
+  CurieIMU.readAccelerometerScaled(accX, accY, accZ);
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY,accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  kalmanX.setAngle(roll); // Set starting angle
+  kalmanY.setAngle(pitch);
+  gyroXangle = roll;
+  gyroYangle = pitch;
+  compAngleX = roll;
+  compAngleY = pitch;
+
+  timer = micros();
+  delay(5000);
+}
+
+void loop() {
+  /* Update all the values */
+ varX = 0;
+varY = 0; 
+
+ for(int a=0;a<200;a++){
+ CurieIMU.readAccelerometerScaled(accX, accY, accZ);
+ CurieIMU.readGyroScaled(gyroX, gyroY, gyroZ);
+
+  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+  timer = micros();
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  double gyroXrate = gyroX / 131.0; // Convert to deg/s
+  double gyroYrate = gyroY / 131.0; // Convert to deg/s
+
+#ifdef RESTRICT_PITCH
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+    kalmanX.setAngle(roll);
+    compAngleX = roll;
+    kalAngleX = roll;
+    gyroXangle = roll;
+  } else
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleX) > 90)
+    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+#else
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+    kalmanY.setAngle(pitch);
+    compAngleY = pitch;
+    kalAngleY = pitch;
+    gyroYangle = pitch;
+  } else
+    kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleY) > 90)
+    gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+#endif
+
+  gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+  gyroYangle += gyroYrate * dt;
+  //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
+  //gyroYangle += kalmanY.getRate() * dt;
+
+  compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+  compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+
+  // Reset the gyro angle when it has drifted too much
+  if (gyroXangle < -180 || gyroXangle > 180)
+    gyroXangle = kalAngleX;
+  if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+
+  /* Print Data */
+#if 0 // Set to 1 to activate
+#endif
+
+
+
+
+
+ if(kalAngleX>(maxX)) maxX=kalAngleX;
+ if(kalAngleX<minX) minX=kalAngleX;
+ if(kalAngleY>maxY) maxY=kalAngleY;
+ if(kalAngleY<minY) minY=kalAngleY;
+
+ varX = abs(maxX-minX);
+ varY = abs(maxY-minY);
+ 
+
+  //Serial.print(roll); Serial.print("\t");
+  //Serial.print(gyroXangle); Serial.print("\t");
+  //Serial.print(compAngleX); Serial.print("\t");
+  Serial.print(kalAngleX); Serial.print("\t");
+  //Serial.print(varX); Serial.print("\t");
+
+  Serial.print("\t");
+
+  //Serial.print(pitch); Serial.print("\t");
+  //Serial.print(gyroYangle); Serial.print("\t");
+  //Serial.print(compAngleY); Serial.print("\t");
+  Serial.print(kalAngleY); Serial.print("\t");
+  //Serial.print(varY); Serial.print("\t");
+
+
+  Serial.print("\r\n");
+  
+ }
+   minX = kalAngleX;
+  minY = kalAngleY;
+  maxX = kalAngleX;
+  maxY = kalAngleY;
+}
