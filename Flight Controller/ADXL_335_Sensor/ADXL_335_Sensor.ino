6fbd32c4d702502507f38fc2765acb5727228b20
Nikhil Mishra <12347532+Curious-Nikhil@users.noreply.github.com>
2018-12-15 02:04:49 +0530

    Simple angle meter using ADXL335 accelerometer
    https://www.electronicsblog.net/simple-angle-meter-using-adxl335-accelerometer-arduino/

--- Flight Controller/ADXL_335_Sensor/ADXL_335_Sensor.ino
+++ Flight Controller/ADXL_335_Sensor/ADXL_335_Sensor.ino
@@ -1,46 +1,63 @@
-// these constants describe the pins. They won't change:
+#define ADC_ref 2.56
+#define zero_x 1.569
+#define zero_y 1.569
+#define zero_z 1.569
+#define sen_x 0.3
+#define sen_y 0.3 
+#define sen_z 0.3
+
 const int xpin = A0;                 // x-axis of the accelerometer
 const int ypin = A1;                  // y-axis
 const int zpin = A2;                  // z-axis (only on 3-axis models)
 const int LED = 13;
 
+float val_x;
+float val_y;
+float val_z;
+
+float xv;
+float yv;
+float zv;
+float angle_x;
+float angle_y;
+float angle_z;
+
 void setup() {
-  // initialize the serial communications:
+  // initialize the serial communications
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
-
-  // Provide ground and power by using the analog inputs as normal digital pins.
-  // This makes it possible to directly connect the breakout board to the
-  // Arduino. If you use the normal 5V and GND pins on the Arduino,
-  // you can remove these lines.
-//  pinMode(groundpin, OUTPUT);
-//  pinMode(powerpin, OUTPUT);
-//  digitalWrite(groundpin, LOW);
-//  digitalWrite(powerpin, HIGH);
 }
 
 void loop() {
- 
-  if (analogRead(zpin) >450) {
-      digitalWrite(LED, HIGH);
-  delay(1000);
-  }else{
-      digitalWrite(LED, LOW);
-  delay(1000);
-  }
+  val_x = analogRead(xpin);
+  val_y = analogRead(ypin);
+  val_z = analogRead(zpin);
 
+  xv = (val_x/1024*ADC_ref - zero_x)/sen_x;
+  yv = (val_z/1024*ADC_ref - zero_y)/sen_y;
+  angle_z = atan2(-yv, -xv)*57.2957795+180;
   
+  Serial.print(xv);
+  Serial.print("\t");
   
-  // print the sensor values:
-  Serial.print(analogRead(xpin));
-  // print a tab between values:
-  Serial.print("\t");
-  Serial.print(analogRead(ypin));
-  // print a tab between values:
-  Serial.print("\t");
-  Serial.print(analogRead(zpin));
-  Serial.println();
-  // delay before next reading:
-  delay(100);
+  Serial.print(angle_z);
+  Serial.print("\n");
+  delay(10);
+
 }
+
+
+//  
+//  // print the sensor values:
+//  Serial.print(analogRead(xpin));
+//  // print a tab between values:
+//  Serial.print("\t");
+//  Serial.print(analogRead(ypin));
+//  // print a tab between values:
+//  Serial.print("\t");
+//  Serial.print(analogRead(zpin));
+//  Serial.println();
+//  // delay before next reading:
+//  delay(100);
+
 
