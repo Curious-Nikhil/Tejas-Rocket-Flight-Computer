a78861c05cf3033ae7c5345525a44435428eb281
Nikhil Mishra <AvatarNikhil@users.noreply.github.com>
2018-12-24 00:59:37 +0530

    Fix #1 and Tests
    Fixed #1
    Tested servomotor
    have to understand that program! :+1:

---
+++ Tejas/ServoMotor_9g_SG90_Test/ServoMotor_9g_SG90_Test.ino
@@ -0,0 +1,52 @@
+/*
+Into Robotics
+*/
+ 
+#include <Servo.h>  //add '<' and '>' before and after servo.h
+ 
+int servoPin = 9;
+ 
+Servo servo;  
+ 
+int servoAngle = 0;   // servo position in degrees
+ 
+void setup()
+{
+  Serial.begin(9600);  
+  servo.attach(servoPin);
+}
+ 
+ 
+void loop()
+{
+//control the servo's direction and the position of the motor
+
+   servo.write(45);      // Turn SG90 servo Left to 45 degrees
+   delay(1000);          // Wait 1 second
+   servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
+   delay(1000);          // Wait 1 second
+   servo.write(135);     // Turn SG90 servo Right to 135 degrees
+   delay(1000);          // Wait 1 second
+   servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
+   delay(1000);
+
+//end control the servo's direction and the position of the motor
+
+
+//control the servo's speed  
+
+//if you change the delay value (from example change 50 to 10), the speed of the servo changes
+  for(servoAngle = 0; servoAngle < 180; servoAngle++)  //move the micro servo from 0 degrees to 180 degrees
+  {                                  
+    servo.write(servoAngle);              
+    delay(50);                  
+  }
+
+  for(servoAngle = 180; servoAngle > 0; servoAngle--)  //now move back the micro servo from 0 degrees to 180 degrees
+  {                                
+    servo.write(servoAngle);          
+    delay(10);      
+  }
+  //end control the servo's speed  
+}
+
