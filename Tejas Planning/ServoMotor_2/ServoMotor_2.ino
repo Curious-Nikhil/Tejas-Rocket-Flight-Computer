fe6cb36c668c4e45b0f1bf71b88814932d003c02
Nikhil Mishra <AvatarNikhil@users.noreply.github.com>
2018-12-31 01:53:32 +0530

    servomotor control monitor
    test control of servo motor from serial monitor! :D

---
+++ Tejas Planning/ServoMotor_2/ServoMotor_2.ino
@@ -0,0 +1,71 @@
+/*
+Into Robotics
+*/
+ 
+#include <Servo.h>  //add '<' and '>' before and after servo.h
+ 
+int servoPin = 9;
+ 
+Servo servo;  
+ 
+int servoAngle = 0;   // servo position in degrees
+ 
+void setup()
+{
+  Serial.begin(9600);  
+  servo.attach(servoPin);
+  Serial.println("Enter Angle");
+
+}
+
+char rx_byte = 0;
+String rx_str = "";
+boolean not_number = false;
+int result;
+
+void loop()
+{
+//control the servo's direction and the position of the motor
+
+//   servo.write(45);      // Turn SG90 servo Left to 45 degrees
+//   delay(5000);          // Wait 1 second
+//   servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
+//   delay(5000);          // Wait 1 second
+//   servo.write(135);     // Turn SG90 servo Right to 135 degrees
+//   delay(5000);          // Wait 1 second
+//   servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
+//   delay(5000);
+
+//https://startingelectronics.org/software/arduino/learn-to-program-course/19-serial-input/
+
+  if (Serial.available() > 0) {    // is a character available?
+    rx_byte = Serial.read();       // get the character
+    
+    if ((rx_byte >= '0') && (rx_byte <= '9')) {
+      rx_str += rx_byte;
+    }
+    else if (rx_byte == '\n') {
+      // end of string
+      if (not_number) {
+        Serial.println("Not a number");
+      }
+      else {
+        Serial.print("Angle: ");
+        Serial.print(rx_str);
+        result = rx_str.toInt(); //converts char to int! :D life is simple with C 
+
+        servo.write(result);
+        
+        Serial.println("");
+        Serial.println("Enter Angle");
+      }
+      not_number = false;         // reset flag
+      rx_str = "";                // clear the string for reuse
+    }
+    else {
+      // non-number character received
+      not_number = true;    // flag a non-number
+    }
+  } // end: if (Serial.available() > 0)
+}
+
--- Tejas Planning/ServoMotor_9g_SG90_Test/ServoMotor_9g_SG90_Test.ino
+++ Tejas Planning/ServoMotor_9g_SG90_Test/ServoMotor_9g_SG90_Test.ino
@@ -22,19 +22,19 @@ void loop()
 //control the servo's direction and the position of the motor
 
    servo.write(45);      // Turn SG90 servo Left to 45 degrees
-   delay(1000);          // Wait 1 second
+   delay(5000);          // Wait 1 second
    servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
-   delay(1000);          // Wait 1 second
+   delay(5000);          // Wait 1 second
    servo.write(135);     // Turn SG90 servo Right to 135 degrees
-   delay(1000);          // Wait 1 second
+   delay(5000);          // Wait 1 second
    servo.write(90);      // Turn SG90 servo back to 90 degrees (center position)
-   delay(1000);
+   delay(5000);
 
 //end control the servo's direction and the position of the motor
 
-
-//control the servo's speed  
-
+//
+////control the servo's speed  
+//
 //if you change the delay value (from example change 50 to 10), the speed of the servo changes
   for(servoAngle = 0; servoAngle < 180; servoAngle++)  //move the micro servo from 0 degrees to 180 degrees
   {                                  
