95bfdb10c723c8a52b13bb224d8dce2a2828a9a7
Nikhil Mishra <12347532+Curious-Nikhil@users.noreply.github.com>
2020-01-09 23:02:47 +0530

    New thrust data

--- Datadump/Thrust Bench/9thJan2020/Jan9th2020.m
+++ Datadump/Thrust Bench/9thJan2020/Jan9th2020.m
@@ -4,15 +4,23 @@ clear all;
 T = readtable('M8_test.csv');
 
 figure
-plot(T.Time, T.Thrust/1000);
+plot(T.Time, (T.Thrust/1000)*9.8);
 xlabel('Time (s)')
-ylabel('Thrust (kg)')
+ylabel('Thrust (N)')
 title('M8 Rocket Motor Test')
 
-T_Impulse = (sum(T.Thrust)/1000)*0.95*9.8; %Ns 
+
+T_Impulse = (sum(T.Thrust)/1000)*0.095*9.8; %Ns 
 S_Imp = T_Impulse/0.050; %50grams Prop
 
 S_Impulse = S_Imp/9.8;
 
 F_avg = T_Impulse/(T.Time(38)); %N
+disp('Total Impulse ');
+disp(T_Impulse);
 
+disp('Specific Impulse ');
+disp(S_Impulse);
+
+disp('Avg Thrust ');
+disp(F_avg);
